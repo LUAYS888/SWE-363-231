827f323752eb5dda409cba64dcec89000c3138fb
Luay Saleh <Loai.saleh20@gmail.com>
2023-11-27 01:34:00 +0300

    BE_3-EX_3

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const path = require('path');
+const router = express.Router();
 
 
 
@@ -10,35 +11,78 @@ app.listen(2000, () => {
 })
 
 //
+// router.get('/', function (req, res, next) {
+//     res.sendFile(path.join(__dirname, '/index.html'));
+//     //next()
+//     res.end();
+// })
+// app.use(router);
 
-app.get("/", (req,res)=>{
-    //res.send("home")
-    res.sendFile(path.join(__dirname, '/index.html'));
-})
+app.route("/")
+    .get((req,res)=>{
+        res.sendFile(path.join(__dirname, '/index.html'));   
+    })
 
-app.get("/AskForAvailabiliy", (req,res)=>{
-    res.sendFile(path.join(__dirname, '/AskForAvailabiliy.html'));
-})
+app.route("/AskForAvailabiliy")
+    .get((req,res)=>{
+        res.sendFile(path.join(__dirname, '/AskForAvailability.html'));   
+    })
 
-app.get("/CarInsurance", (req,res)=>{
-    res.sendFile(path.join(__dirname, '/CarInsurance.html'));
-})
+app.route("/CarInsurance")
+    .get((req,res)=>{
+        res.sendFile(path.join(__dirname, '/CarInsurance.html'));   
+    }) 
+    
+app.route("/contactPage")
+    .get((req,res)=>{
+        res.sendFile(path.join(__dirname, '/contactPage.html'));   
+    })
 
-app.get("/contactPage", (req,res)=>{
-    res.sendFile(path.join(__dirname, '/contactPage.html'));
-})
+app.route("/drivingTest")
+    .get((req,res)=>{
+        res.sendFile(path.join(__dirname, '/drivingTest.html'));   
+    })    
 
-app.get("/drivingTest", (req,res)=>{
-    res.sendFile(path.join(__dirname, '/drivingTest.html'));
-})
+app.route("/requestCar")
+    .get((req,res)=>{
+        res.sendFile(path.join(__dirname, '/requestCar.html'));   
+    })
 
-app.get("/requestCar", (req,res)=>{
-    res.sendFile(path.join(__dirname, '/requestCar.html'));
-})
+app.route("/requestMaintenance")
+    .get((req,res)=>{
+        res.sendFile(path.join(__dirname, '/requestMaintenance.html'));   
+    })
+    
 
-app.get("/requestMaintenance", (req,res)=>{
-    res.sendFile(path.join(__dirname, '/requestMaintenance.html'));
-})
+
+// app.get("/", (req,res)=>{
+//     //res.send("home")
+//     res.sendFile(path.join(__dirname, '/index.html'));
+// })
+
+// app.get("/AskForAvailabiliy", (req,res)=>{
+//     res.sendFile(path.join(__dirname, '/AskForAvailabiliy.html'));
+// })
+
+// app.get("/CarInsurance", (req,res)=>{
+//     res.sendFile(path.join(__dirname, '/CarInsurance.html'));
+// })
+
+// app.get("/contactPage", (req,res)=>{
+//     res.sendFile(path.join(__dirname, '/contactPage.html'));
+// })
+
+// app.get("/drivingTest", (req,res)=>{
+//     res.sendFile(path.join(__dirname, '/drivingTest.html'));
+// })
+
+// app.get("/requestCar", (req,res)=>{
+//     res.sendFile(path.join(__dirname, '/requestCar.html'));
+// })
+
+// app.get("/requestMaintenance", (req,res)=>{
+//     res.sendFile(path.join(__dirname, '/requestMaintenance.html'));
+// })
 
 
 
