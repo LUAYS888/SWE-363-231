2c6441e218ed60bca6c366079985f2c3b62f0973
Luay Saleh <Loai.saleh20@gmail.com>
2023-12-06 12:23:02 +0300

    Sec01Ex

--- server.js
+++ server.js
@@ -20,10 +20,18 @@ app.listen(2000, () => {
 // })
 // app.use(router);
 
-app.route("/")
-    .get((req,res)=>{
-        res.sendFile(path.join(__dirname, '/index.html'));   
-    })
+app.get("/",authentication,(req,res)=>{
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+
+
+
+
+// app.route("/",authentication)
+//     .get((req,res)=>{
+//         res.sendFile(path.join(__dirname, '/index.html'));   
+//     })
 
 app.route("/AskForAvailability")
     .get((req,res)=>{
@@ -64,6 +72,40 @@ app.route("/requestMaintenance")
     .get((req,res)=>{
         res.sendFile(path.join(__dirname, '/requestMaintenance.html'));   
     })
+
+
+
+
+
+    function authentication(req, res, next) {
+        const authheader = req.headers.authorization;
+        console.log(req.headers);
+    
+        if (!authheader) {
+            let err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err)
+        }
+    
+        const auth = new Buffer.from(authheader.split(' ')[1],
+            'base64').toString().split(':');
+        const user = auth[0];
+        const pass = auth[1];
+    
+        if (user == 'Luay' && pass == 'pass222') {
+    
+           
+            next();
+        } else {
+            let err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err);
+        }
+    
+    }
+    
     
 
 
